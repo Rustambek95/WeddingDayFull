3946480888bc0e44abf8b2c2b4a77fd7e5e0362f
Rustambek95 <107795505+Rustambek95@users.noreply.github.com>
2025-08-28 16:04:19 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,26 +7,26 @@ function autoplay() {
 
 function IntervalDate() {
     const wedding = new Date().getTime()
-    const weddingDay = new Date('august 28, 2022 00:00:00')
+    const weddingDay = new Date('august 28, 2022 18:00:00')
     let interval = wedding - weddingDay 
 
     const sec = 1000
     const min = 60 * sec
     const hour = 60 * min
     const day = 24 * hour
-    // const week = 7.11 * day
-    // const year = 365.25 * day
+    //const week = 7.11 * day
+    const year = 365.25 * day
 
 
-    // const yearInterval = Math.floor(interval / year)
-    // const weekInterval = Math.floor(interval / week)
-    const daysInterval = Math.floor(interval / day)
+    const yearInterval = Math.floor(interval / year)
+    //const weekInterval = Math.floor(interval / week)
+    const daysInterval = Math.floor(interval % year / day)
     const hoursInterval = Math.floor(interval % day / hour)
     const minInterval = Math.floor(interval % hour / min)
     const secInterval = Math.floor(interval % min / sec)
 
-    // document.querySelector('.year').textContent = yearInterval
-    // document.querySelector('.week').textContent = weekInterval
+    document.querySelector('.year').textContent = yearInterval
+    //document.querySelector('.week').textContent = weekInterval
     document.querySelector('.day').textContent = daysInterval
     document.querySelector('.hour').textContent = hoursInterval
     document.querySelector('.min').textContent = minInterval
